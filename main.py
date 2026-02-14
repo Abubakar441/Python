34f3c92fdacf706f3af40080c10ae3830756b3f1
Abubakar441 <bakerjutthaider@gmail.com>
2026-02-14 15:17:33 +0500

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
 print("my frist output")
+print("""for
+show
+in next line""")
+print("for next line \n like this")
+print('it\'s will wrok')
+"""it is also used for quatations """
+#for inout from user: 
+name= input("enyer your name:")
+print (name)
+age=int(input("enter you age:"))
+print(age)
+#for simple plus minus and others (eval)
+add=eval(input("enter your equation:"))
+print(add)
+# for know your datatype
+name="ali"
+age=12
+print((type(name)) ,(type (age)))
+#for type casting
+a=11
+b="23.5"
+print (type(a))
+print (type(b))
+b = float(b)
+c=a+b
+print(c)
+print(type(c))
+#for swap:
+x=12
+y=20
+#for swap
+z=x
+x=y
+y=z
+print(y)
+print(x)
+#method 2
+a=15
+b=17
+a,b=b,a
+print(a)
+print(b)
+#experiment for print both at same time but in different line
+name = input("enter your name:")
+name_2=input("enetr adain:")
+print(name+ "\n" + name_2)
+print ("""name
+name_2""")
+
+
